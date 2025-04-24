6ff471759a04aed183ecc67c0d201ad7297def71
eduardorochadev <chicoedumacedo@gmail.com>
2025-04-23 23:33:07 -0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { BannerComponent } from "./components/banner/banner.component";
+import { ServicesComponent } from "./components/services/services.component";
+import { PortfolioComponent } from "./components/portfolio/portfolio.component";
+import { FooterComponent } from "./components/footer/footer.component";
+import { NavbarComponent } from "./components/navbar/navbar.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, BannerComponent, ServicesComponent, PortfolioComponent, FooterComponent, NavbarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
