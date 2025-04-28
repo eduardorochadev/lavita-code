9ea1a099b454b5b7b4e7deef7a39bb920a79c144
eduardorochadev <chicoedumacedo@gmail.com>
2025-04-28 15:49:17 -0300

    correção dos componentes no typescript

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -1,12 +1,25 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [],
+  imports: [ CommonModule],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.scss'
 })
 export class FooterComponent {
+  contactTitle = 'Contact Us';
 
+  contactInfo = {
+    email: 'contact@lavitacode.com',
+    phone: '+1 (123) 456-7890',
+    location: '123 Main St, City, Country'
+  };
+
+  socials = [
+    { name: 'Facebook', url: '#' },
+    { name: 'Twitter', url: '#' },
+    { name: 'LinkedIn', url: '#' }
+  ];
 }
--- src/app/components/services/services.component.ts
+++ src/app/components/services/services.component.ts
@@ -1,12 +1,29 @@
 import { Component } from '@angular/core';
-
+import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-services',
   standalone: true,
-  imports: [],
+  imports: [ CommonModule],
   templateUrl: './services.component.html',
   styleUrl: './services.component.scss'
 })
 export class ServicesComponent {
-
+  sectionTitle = 'Nossos Serviços';
+  services = [
+    {
+      icon: '📱',
+      title: 'App Development',
+      description: 'Creating seamless and user-friendly mobile applications tailored to your needs.'
+    },
+    {
+      icon: '🌐',
+      title: 'Web Development',
+      description: 'Building responsive and dynamic websites that engage and convert visitors.'
+    },
+    {
+      icon: '⚙️',
+      title: 'System Integration',
+      description: 'Integrating various systems to ensure seamless operations and improved efficiency.'
+    }
+  ];
 }
